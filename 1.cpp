5fd88c05bec19cadae66b4ddcf6dd3fa2dd675a1
GabrielBravin <gabrielbravin89@gmail.com>
2015-05-23 19:03:01 -0300

    Acrecentando 0
    Teste

--- 1.cpp
+++ 1.cpp
@@ -24,7 +24,7 @@ bool eTriangulo(float ladoA, float ladoB, float ladoC){
     if ( ( ladoA < (ladoB + ladoC) ) && ( ladoB < ( ladoA + ladoC)  ) && ( ladoC < ( ladoA + ladoB ) ) ){
         return true;
     }else{
-        return false;
+        return false (0);
     }
 }
 */
